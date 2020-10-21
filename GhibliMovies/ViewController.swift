1e038c66b5b7dabaa371364816e7b3e164662e18
Clément Cardonnel <clement.cardonnel@me.com>
2020-10-21 14:46:13 +0200

    Initial Commit

---
+++ GhibliMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GhibliMovies
+//
+//  Created by Clément Cardonnel on 21/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
